960bbfc35bc6ee6a2bdf49560c0b8df475d99aa9
Reid Shipley <r.w.shipley7@gmail.com>
2025-03-12 15:31:36 -0500

    new file created

---
+++ weatherApiExample.js
@@ -0,0 +1,18 @@
+//Weather_API
+
+//README
+//API_Key: 8c1dd98738c94a3ebc6c821421c0365c
+
+//Endpoint_for_data_retrieval:
+//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+//Endpoint_with_Dallas_coords_and_api_key:
+//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+//direct_geoconversion_for_longitute_and_lattitude: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+const apiKey = prompt("please input your API key from the openweathermap api service: ");
+
+const city = prompt("please input your city with an uppercase: ")
+
+const apiEndpoint = (apiKey, lat, lon) => a / b;
